06f679c9d0064bef4d7e22031e4d8a541a4ccecb
ingkebil <kennybilliau@gmail.com>
2014-10-07 22:11:21 +0200

    Got to comparing EnsEMBL with client data

--- scripts/find_hg_version.py
+++ scripts/find_hg_version.py
@@ -10,7 +10,7 @@ def main(argv):
     cur = conn.cursor()
 
     # first get the list of available human dbs
-    cur.execute("show databases like 'homo_sapiens_core%'")
+    cur.execute("show databases like 'homo_sapiens_core\_%'")
     rs = cur.fetchall()
     dbs = [ db[0] for db in rs ]
 
@@ -18,10 +18,13 @@ def main(argv):
     for db in dbs:
         cur.execute("use %s;" % db)
         try:
+            # TODO check with other query for releases 48 - 64
             cur.execute("select g.stable_id, g.seq_region_start, g.seq_region_end from gene g join seq_region using (seq_region_id) where g.seq_region_start = %s and g.seq_region_end = %s AND g.stable_id = %s", (argv[0], argv[1], argv[2]))
             rs = cur.fetchall()
             if len(rs) != 0:
                 print("%s is the one" % db)
+            else:
+                print("%s is NOT the one" % db)
         except InternalError as ierrr:
             if ierrr.args[0] != 1054:
                 raise
