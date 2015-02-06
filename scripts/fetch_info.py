290e9df7417ac1572f10e19bf4d6f060ac87886a
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-02-06 20:50:27 +0100

    Added OMIM morbid when missing

--- scripts/fetch_info.py
+++ scripts/fetch_info.py
@@ -444,6 +444,10 @@ def query_omim(data):
             terms = (TERMS_MAPPER.get(model_human, model_human) for model_human in models)
             line['Genetic_disease_model'] = ','.join(terms)
 
+            if 'OMIM_morbid' in line and len(line['OMIM_morbid']) > 0 and line['OMIM_morbid'] != entry['mim_number']:
+                p('%s > %s client OMIM number differs from OMIM query')
+            line['OMIM_morbid'] = entry['mim_number']
+
             sleep(0.25) # wait for 250ms as according to OMIM specs
         yield line
 
