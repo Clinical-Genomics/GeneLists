119944382a17d6d2643bc19b42abd4a4151b1f1f
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-02-19 11:54:05 +0100

    Improved handling of RefSeq IDs

--- scripts/fetch_info.py
+++ scripts/fetch_info.py
@@ -127,7 +127,7 @@ def query(data, try_hgnc_again=False):
     Yields:
         dict: a row with data from ensEMBLdb filled in.
     """
-    conn = pymysql.connect(host='ensembldb.ensembl.org', port=5306, user='anonymous', db='homo_sapiens_core_75_37')
+    global conn
     cur = conn.cursor(pymysql.cursors.DictCursor)
 
     base_query = """
@@ -221,6 +221,18 @@ def query_transcripts(data):
             return description
         return None
 
+    def _join_refseqs(transcripts):
+        transcripts_refseqs = []
+        for transcript in sorted(transcripts.keys()):
+            refseqs = '/'.join(sorted([ refseq for refseq in transcripts[ transcript ] if refseq != None ]))
+
+            if len(refseqs) == 0:
+                transcripts_refseqs.append(transcript)
+            else:
+                transcripts_refseqs.append('%s>%s' % (transcript, refseqs))
+
+        return transcripts_refseqs
+
     def _process_transcripts(data):
         """Processes raw data:
         * aggregates transcripts, RefSeq IDs
@@ -237,7 +249,7 @@ def query_transcripts(data):
         Ensembl_ID = row['Ensembl_ID']
         line = {} # will only hold two keys: Ensembl_transcript_to_refseq_transcript and Gene_description
         prev_description = _cleanup_description(row['description'])
-        transcripts = ['%s>%s' % (row['Transcript_ID'], row['RefSeq_ID'])]
+        transcripts = { row['Transcript_ID']: [ row['RefSeq_ID'] ] }
 
         for row in data:
             if row['Ensembl_ID'] != Ensembl_ID:
@@ -245,25 +257,24 @@ def query_transcripts(data):
                 if len(transcripts) == 0:
                     p(Ensembl_ID + ' has no transcripts!')
 
-                line['Ensembl_transcript_to_refseq_transcript'] = '%s:%s' % (Ensembl_ID, '|'.join(transcripts))
+                line['Ensembl_transcript_to_refseq_transcript'] = '%s:%s' % (Ensembl_ID, '|'.join(_join_refseqs(transcripts)))
                 line['Gene_description'] = prev_description
 
                 yield line
 
                 # reset
-                transcripts = []
+                transcripts = {}
                 Ensembl_ID = row['Ensembl_ID']
 
                 line = {}
                 prev_description = _cleanup_description(row['description'])
 
-            if row['RefSeq_ID'] == None:
-                p('%s:%s has no RefSeqID' % (Ensembl_ID, row['Transcript_ID']))
-
-            transcripts.append('%s>%s' % (row['Transcript_ID'], row['RefSeq_ID']))
+            if row['Transcript_ID'] not in transcripts:
+                transcripts[ row['Transcript_ID'] ] = []
+            transcripts[ row['Transcript_ID'] ].append(row['RefSeq_ID'])
 
         # yield last one
-        line['Ensembl_transcript_to_refseq_transcript'] = '%s:%s' % (Ensembl_ID, '|'.join(transcripts))
+        line['Ensembl_transcript_to_refseq_transcript'] = '%s:%s' % (Ensembl_ID, '|'.join(_join_refseqs(transcripts)))
         line['Gene_description'] = prev_description
         yield line
 
@@ -274,7 +285,7 @@ def query_transcripts(data):
 
     for line in data:
         base_query = """
-        SELECT g.seq_region_start AS Gene_start, g.seq_region_end AS Gene_stop,
+        SELECT DISTINCT g.seq_region_start AS Gene_start, g.seq_region_end AS Gene_stop,
         x.display_label AS HGNC_symbol, g.stable_id AS Ensembl_ID,
         seq_region.name AS Chromosome, t.stable_id AS Transcript_ID, g.description,
         tx.dbprimary_acc AS RefSeq_ID
@@ -282,20 +293,21 @@ def query_transcripts(data):
         JOIN xref x ON x.xref_id = g.display_xref_id
         JOIN seq_region USING (seq_region_id)
         LEFT JOIN transcript t ON t.gene_id = g.gene_id
-        LEFT JOIN object_xref ox ON ox.ensembl_id = t.transcript_id
-        LEFT JOIN xref tx ON tx.xref_id = ox.xref_id
+        LEFT JOIN object_xref ox ON ox.ensembl_id = t.transcript_id AND ox.ensembl_object_type = 'Transcript'
+        LEFT JOIN xref tx ON tx.xref_id = ox.xref_id AND tx.external_db_id in (1801, 1806, 1810)
         WHERE length(seq_region.name) < 3
-        AND tx.external_db_id in (1801, 1806, 1810)
         AND g.stable_id = %s
+        ORDER BY g.gene_id, t.transcript_id
         """
 
         cur.execute(base_query, line['Ensembl_gene_id'])
         rs = cur.fetchall()
-        transcripts = _process_transcripts(rs)
+        if len(rs) > 0:
+            transcripts = _process_transcripts(rs)
 
-        for transcript in transcripts:
-            line['Ensembl_transcript_to_refseq_transcript'] = transcript['Ensembl_transcript_to_refseq_transcript']
-            line['Gene_description'] = transcript['Gene_description']
+            for transcript in transcripts:
+                line['Ensembl_transcript_to_refseq_transcript'] = transcript['Ensembl_transcript_to_refseq_transcript']
+                line['Gene_description'] = transcript['Gene_description']
 
         yield line
 
