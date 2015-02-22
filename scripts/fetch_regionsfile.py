ef7b4f62fda69a6d0c81c3f98353ff8bbaed17b3
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-02-22 21:09:02 +0100

    test output regions file

--- scripts/fetch_regionsfile.py
+++ scripts/fetch_regionsfile.py
@@ -8,6 +8,8 @@ import re
 
 from .ensembl import Ensembl
 
+header = ['Chromosome', 'Gene_start', 'Gene_stop', 'Ensembl_gene_id', 'HGNC_symbol', 'Ensembl_transcript_to_refseq_transcript', 'Gene_description']
+
 def fill_line(row):
     """Formats a line in the regions file with information found in the row
 
@@ -30,14 +32,26 @@ def fill_line(row):
 
     return '\t'.join([row['Chromosome'], row['Gene_start'], row['Gene_stop'], row['Ensembl_gene_id'], row['HGNC_symbol'], row['Ensembl_transcript_to_refseq_transcript'], row['Gene_description']])
 
+def get_lines(transcripts):
+    """Generate the regions file, line by line, including header
+
+    Args:
+        transcripts (list): list of dicts. Dict keys are equal to the header keys.
+
+    Yield: all lines, including header, of the regions file
+    """
+
+    yield '#' + '\t'.join(header)
+    for transcript in transcripts:
+        yield fill_line(transcript)
+
 def main(argv):
 
     with Ensembl() as ensembldb:
         transcripts = ensembldb.query_transcripts()
 
-    print('#Chromosome	Gene_start	Gene_stop	Ensembl_gene_id	HGNC_symbol	Ensembl_transcript_to_refseq_transcript	Gene_description')
-    for transcript in transcripts:
-        print(fill_line(transcript))
+    for line in get_lines(transcripts):
+        print(line)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
