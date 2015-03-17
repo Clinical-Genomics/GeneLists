bf2932f7f94cb4e85596f82bab6c2bacb70c65f6
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-03-17 10:02:50 +0100

    Add panels to cg.se page

--- scripts/acronyms.py
+++ scripts/acronyms.py
@@ -1,35 +1,78 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+# TODO load the acronyms from a folder provided through a config file
+
 from __future__ import print_function
+import re
 import sys
 import glob
 
 class Acronyms(object):
 
     def __init__(self, base_dir):
-        """Reads in the LISTS files from the git repo's
+        """Reads in the LISTS files from the git repo's to get gene list and panel descriptions.
+        Reads in all gene lists to gather all the panels for each gene list. Will fail silently
+        when no panels are found or on faulty lines in the gene list.
 
         Args:
             base_dir (str): full path to the basedir of the repo's
         """
+
         self.acronyms = {} # acro: long text
+        self.panels   = {} # acro: list of panels (acro)
+
         for list_file in glob.glob(base_dir + '/*/LISTS'):
             f = open(list_file)
             for line in f.readlines():
                 acro, sep, full_name = line.strip().partition(': ')
                 self.acronyms[acro] = full_name
 
+        re_gl_name = re.compile(r'cust...-(.*).txt')
+        for gl in glob.glob(base_dir + '/*/cust*txt'):
+            gl_name = re.search(re_gl_name, gl).group(1)
+
+            with open(gl, 'r') as f:
+                lines = (line.strip() for line in f.readlines())
+                panels = {}
+
+                for line in lines:
+                    # skip comments
+                    if line.startswith('#'):
+                        continue
+
+                    l = line.split('\t')
+
+                    if len(l) < 18: # skip faulty lines silently
+                        continue
+
+                    cur_panels = l[17].split(',')
+                    cur_panels = ( panel.strip() for panel in cur_panels )
+                    for cur_panel in cur_panels:
+                        panels[cur_panel] = 1
+
+                self.panels[gl_name] = panels.keys()
+
     def __getitem__(self,  acronym):
-        """TODO: Docstring for __item__.
+        """Retrieve the acronym description
 
         Args:
-            acronym (TODO): TODO
+            acronym (str): a gene list/panel acronym
 
-        Returns: TODO
+        Returns: acronym description
 
         """
         return self.acronyms.get(acronym, '')
+
+    def get_panels_of(self, acronym):
+        """Retrieve the panels of a gene list
+
+        Args:
+            acronym (str): a gene list acronym.
+
+        Returns: a list of panels (acronyms)
+        """
+        return self.panels.get(acronym, [])
 
 def main(args):
     acrs = Acronyms(args[0])
--- scripts/update_cg.py
+++ scripts/update_cg.py
@@ -12,6 +12,27 @@ from datetime import datetime
 from .git import getgitlastmoddate, getgittag
 from .acronyms import Acronyms
 
+def panels_2_html(panels, acronyms):
+    """Converts a list of panel acronyms to a HTML list.
+    Will add the description of the panel to the list as well.
+
+    Args:
+        panels (list): a list of panel acronyms
+
+    Returns: a HTML formatted string.
+
+    """
+    if len(panels) == 0:
+        return ''
+
+    li_panels = []
+    for panel in panels:
+        descr = acronyms[panel]
+        panel_descr = panel if len(descr) == 0 else ': '.join([panel, descr])
+        li_panels.append('<li>%s</li>' % panel_descr)
+
+    return '<ul>%s</ul>' % ''.join(li_panels)
+
 def main(argv):
     parser = argparse.ArgumentParser(description='Update clinicalgenomics.se with gene list names and versions')
     parser.add_argument('infiles', nargs="+", type=argparse.FileType('r'), help='')
@@ -20,7 +41,7 @@ def main(argv):
     acronyms = Acronyms(os.path.dirname(os.path.dirname(os.path.realpath(args.infiles[0].name))))
     re_gl_name = re.compile(r'cust...-(.*).txt')
 
-    versions = {} # Database => { 'Version': Version, 'Date': Date, 'Fullname': 'Database': database }
+    versions = {} # Database => { 'Version': Version, 'Datum': Date, 'Beskrivning': description, 'Databas': database acronym}
     for infile in args.infiles:
 
         # get the database name from the filename
@@ -31,11 +52,15 @@ def main(argv):
         mod_date = getgitlastmoddate(infile.name, '%Y-%m-%d %H:%M:%S')
         version = getgittag(infile.name, date=mod_date) # get version on that date
         full_name = acronyms[database]
+        panels = acronyms.get_panels_of(database)
+        panels = '' if len(panels) == 1 else panels_2_html(panels, acronyms)
+        if len(panels):
+            panels = '<br />' + panels
         versions[database] = {
             'Version': version,
             'Datum': mod_date.partition(' ')[0],
-            'Beskrivning': full_name,
-            'Databas': database
+            'Beskrivning': full_name + panels,
+            'Databas': database,
         }
 
     print("""---
