bafb36d9bd4c8ae8f0e9707486d895004445d99e
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-15 09:53:51 +0200

    Fix white space in genenames.py

--- scripts/genenames.py
+++ scripts/genenames.py
@@ -8,46 +8,46 @@ import time
 
 class Genenames(object):
     """Basic interface to the public genenames API.
-  
+
     Args:
         response_format (str): format for response (xml, json, etc.)
     """
-  
+
     def __init__(self, response_format='application/json'):
         super(Genenames, self).__init__()
         self.base_url = 'http://rest.genenames.org/'
         self.format = response_format
-    
+
         requests_cache.install_cache('genenames_cache', backend='sqlite', expire_after=846000)
-  
+
     def get(self, handler):
         """Compose url and universal headers for any request handler.
-    
+
         Args:
             handler (str): API entry point
-    
+
         Returns (json): a parsed json object
         """
         url = "%s/%s" % (self.base_url, handler)
         headers = {
             'Accept': self.format
         }
-    
+
         res = requests.get(url, headers=headers)
-    
+
         if not res.from_cache:
             time.sleep(0.25) # wait for 250ms, play nice
-    
+
         return res.json()
-  
+
     def aliases(self, hgnc_symbol):
       """Fetches the HGNC aliases
-  
+
       Args:
           hgnc_symbol (str): an HGNC symbol.
-  
+
       Returns (list): a list of HGNC symbols
-  
+
       """
       data = self.get("fetch/symbol/%s" % hgnc_symbol)
       try:
@@ -56,17 +56,17 @@ class Genenames(object):
           return None
       except IndexError:
           return None
-  
+
       return aliases
-  
+
     def official(self, hgnc_symbol):
       """Fetches the HGNC official symbol.
-  
+
       Args:
           hgnc_symbol (str): an HGNC symbol.
-  
+
       Returns (str): the official HGNC symbol
-  
+
       """
       data = self.get("fetch/prev_symbol/%s" % hgnc_symbol)
       try:
@@ -75,7 +75,7 @@ class Genenames(object):
           return None
       except IndexError:
           return None
-  
+
       return official_symbol
 
 if __name__ == '__main__':
